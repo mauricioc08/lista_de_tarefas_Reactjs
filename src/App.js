ed3ce2f6a73134e1a297f7063b0c3f134fe44e85
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-06-05 15:42:47 -0300

    config routes and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import RoutesApp from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Register/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <h1>Pagina de Registro</h1>;
+};
+
+export default index;
