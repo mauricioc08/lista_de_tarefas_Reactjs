22a432edda8740202c547ca0a1b1705095066af1
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-06-06 12:13:42 -0300

    add logout and styles admin page

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,47 @@
 import "./admin.css";
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../../fireBaseConnections";
+import { signOut } from "firebase/auth";
 
 const Admin = () => {
+  const [tarefaInput, setTarefaInput] = useState("");
+
+  function handleRegister(e) {
+    e.preventDefault();
+    alert("clicouuuu");
+  }
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
+
   return (
-    <div>
-      <h1>Pagina Admin!</h1>
+    <div className="admin-container">
+      <h1>Minhas Tarefas</h1>
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        />
+        <button className="btn-register" type="submit">
+          Registrar tarefa
+        </button>
+      </form>
+
+      <article className="list">
+        <p>
+          cbhdscbhsdcbodcnkqcnbnvcqpçojwojddddddddddddddddddddddddddddddddddddddddo
+        </p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>
+        Sair
+      </button>
     </div>
   );
 };
